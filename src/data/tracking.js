d414080516b8a2a095edb61c6b9f4168fe91fb83
lassiecoder <sharmapriyanka84510@gmail.com>
2024-04-03 22:29:57 +0530

    misc

--- src/data/tracking.js
+++ src/data/tracking.js
@@ -1,3 +1,3 @@
 // Google Analytics Tracking ID
-// https://github.com/truethari/reactfolio#-google-analytics
+// https://github.com/truethari/portfolio#-google-analytics
 export const TRACKING_ID = "";
