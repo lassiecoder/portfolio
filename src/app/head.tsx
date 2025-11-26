5fa60ecad956a0f81b1df21aa11f8c99425fb766
lassiecoder <sharmapriyanka84510@gmail.com>
2025-11-26 17:29:30 +0530

    feat: added link-preview metadata

---
+++ src/app/head.tsx
@@ -0,0 +1,33 @@
+export default function Head() {
+  return (
+    <>
+      <title>lassiecoder&apos;s portfolio</title>
+      <meta
+        name="Priyanka Sharma (lassiecoder)"
+        content="Crafting compelling narratives through design, merging creativity with functionality seamlessly."
+      />
+
+      {/* Open Graph */}
+      <meta property="og:title" content="Priyanka Sharma (lassiecoder)" />
+      <meta
+        property="og:description"
+        content="She/her 👩🏻‍💼 | AWS re:Invent All Builders Welcome Grant 2025 | 5+ Years #BuildingInTech | #JavaScript | #TypeScript | #ReactNative | #ReactJS | #TechnicalWriter"
+      />
+      <meta
+        property="og:image"
+        content="https://avatars.githubusercontent.com/u/17312616?v=4"
+      />
+      <meta property="og:url" content="https://lassiecoder.com" />
+      <meta property="og:type" content="portfolio" />
+
+      {/* Twitter */}
+      {/* <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:title" content="humanise’s Portfolio" />
+      <meta
+        name="twitter:description"
+        content="Developer portfolio with projects and experience."
+      />
+      <meta name="twitter:image" content="https://yourdomain.com/preview.png" /> */}
+    </>
+  );
+}
